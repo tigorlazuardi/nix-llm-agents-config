056215aac231dfa3573840f4b0bd8e4e556f24e5
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-07-29 06:00:23 +0700

    fix(pi-rules): update to 0.5.4

--- packages/pi-rules.nix
+++ packages/pi-rules.nix
@@ -6,20 +6,20 @@
 }:
 buildNpmPackage {
   pname = "pi-rules";
-  version = "0.5.3";
+  version = "0.5.4";
 
   src = fetchurl {
-    url = "https://registry.npmjs.org/@tigorhutasuhut/pi-rules/-/pi-rules-0.5.3.tgz";
-    hash = "sha256-cagn9JeU+eP2ZElECZaDswLC8lYfR5h7Bbn2NK3YYQU=";
+    url = "https://registry.npmjs.org/@tigorhutasuhut/pi-rules/-/pi-rules-0.5.4.tgz";
+    hash = "sha256-vBfv9Zh08xnOJAxVWnjSWWGqPF84Hk10ViOHIxovF5E=";
   };
 
   # ponytail: omit build-only and host Pi packages; offline load check proves peer resolution.
   postPatch = ''
     cp ${./pi-rules-package-lock.json} package-lock.json
-    ${nodejs}/bin/node -e 'const fs = require("fs"); const p = require("./package.json"); p.dependencies = { ...(p.dependencies || {}), typebox: "1.1.38" }; delete p.devDependencies; delete p.peerDependencies; fs.writeFileSync("package.json", JSON.stringify(p, null, 2) + "\n")'
+    ${nodejs}/bin/node -e 'const fs = require("fs"); const p = require("./package.json"); delete p.devDependencies; delete p.peerDependencies; fs.writeFileSync("package.json", JSON.stringify(p, null, 2) + "\n")'
   '';
 
-  npmDepsHash = "sha256-tticJ+LqQ+pO5V0pxo7aRhJNAJPOOwIWzw4a6L2PV/8=";
+  npmDepsHash = "sha256-umptz1C77AHcLJCtT0fCVKsbu1tiiLL0fZ22MWSrVIk=";
   npmInstallFlags = [
     "--omit=dev"
     "--omit=peer"
