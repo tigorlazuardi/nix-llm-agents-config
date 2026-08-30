250f97e9406d68a8f65afbec9808db4b7f4d7a16
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-08-31 03:08:40 +0700

    fix(pi-diet-lsp): use updater-compatible source hash

--- packages/pi-diet-lsp.nix
+++ packages/pi-diet-lsp.nix
@@ -1,20 +1,24 @@
 {
-  fetchzip,
+  fetchurl,
   lib,
+  runCommand,
 }:
 let
   lock = (import ./pi-plugin-lock.nix)."pi-diet-lsp";
-in
-fetchzip {
-  pname = "pi-diet-lsp";
-  version = lock.version;
-
-  url = lock.src;
-  hash = lock.hash;
-
-  meta = {
-    description = "On-demand LSP code-intelligence tools for Pi";
-    homepage = "https://github.com/ProbabilityEngineer/pi-diet-lsp";
-    license = lib.licenses.mit;
+  src = fetchurl {
+    url = lock.src;
+    hash = lock.hash;
   };
-}
+in
+runCommand "pi-diet-lsp-${lock.version}"
+  {
+    meta = {
+      description = "On-demand LSP code-intelligence tools for Pi";
+      homepage = "https://github.com/ProbabilityEngineer/pi-diet-lsp";
+      license = lib.licenses.mit;
+    };
+  }
+  ''
+    mkdir -p "$out"
+    tar -xzf ${src} --strip-components=1 -C "$out"
+  ''
